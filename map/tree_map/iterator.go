999764e718a23d008c292088e601ab32edc2e59f
Greenpools Ryan <greenpoo@at-k4.msk.21-school.ru>
2024-03-20 20:23:35 +0300

    add treemap

---
+++ map/tree_map/iterator.go
@@ -0,0 +1,41 @@
+package treemap
+
+func (t *treeMap[T, K]) Iterator() *Iterator[T, K] {
+	return &Iterator[T, K]{
+		iterator: t.tree.Iterator(),
+	}
+}
+
+func (i *Iterator[T, K]) Next() bool {
+	return i.iterator.Next()
+}
+
+func (i *Iterator[T, K]) Prev() bool {
+	return i.iterator.Prev()
+}
+
+func (i *Iterator[T, K]) Value() K {
+	return i.iterator.Value()
+}
+
+func (i *Iterator[T, K]) Key() T {
+	return i.iterator.Key()
+}
+
+func (i *Iterator[T, K]) Start() {
+	i.iterator.Start()
+}
+
+func (i *Iterator[T, K]) End() {
+	i.iterator.End()
+}
+
+func (i *Iterator[T, K]) First() bool {
+	i.iterator.Start()
+	return i.iterator.Next()
+}
+
+func (i *Iterator[T, K]) Last() bool {
+	i.iterator.End()
+	return i.iterator.Prev()
+}
--- tree/redblacktree/tree.go
+++ tree/redblacktree/tree.go
@@ -7,68 +7,71 @@ const (
 	start, proccess, end position = 0, 1, 2
 )
 
-type position byte
+type (
+	position byte
 
-type Entry[T any, K any] struct {
-	Key   T
-	Value K
-}
+	Entry[T any, K any] struct {
+		Key   T
+		Value K
+	}
 
-type Node[T any, K any] struct {
-	key                 T
-	value               K
-	left, right, parent *Node[T, K]
-	color               bool
-}
+	Node[T any, K any] struct {
+		key                 T
+		value               K
+		left, right, parent *Node[T, K]
+		color               bool
+	}
 
-type Iterator[T any, K any] struct {
-	tree *RedBlackTree[T, K]
-	node *Node[T, K]
-	position
-}
+	Iterator[T any, K any] struct {
+		tree *RedBlackTree[T, K]
+		node *Node[T, K]
+		position
+	}
 
-type RedBlackTree[T any, K any] struct {
-	root   *Node[T, K]
-	length int
-	cmp    func(T, T) int
-}
+	RedBlackTree[T any, K any] struct {
+		root   *Node[T, K]
+		length int
+		cmp    func(T, T) int
+	}
 
-type Tree[T any, K any] interface {
-	Root() *Node[T, K] // O(1)
-	Size() int         // O(1)
-	Clear()            // O(1)
-	IsEmpty() bool     // O(1)
+	Tree[T any, K any] interface {
+		Root() *Node[T, K] // O(1)
+		Size() int         // O(1)
+		Clear()            // O(1)
+		IsEmpty() bool     // O(1)
+		// Copy() Tree[T, K]
 
-	Insert(key T, value K) *Node[T, K] // O(log(n))
-	Remove(key T) bool                 // O(log(n))
-	Update(key T, value K) bool        // O(log(n))
-	GetNode(key T) (*Node[T, K], bool) // O(log(n))
-	Get(key T) (K, bool)               // O(log(n))
+		Insert(key T, value K) *Node[T, K] // O(log(n))
+		Remove(key T) bool                 // O(log(n))
+		Update(key T, value K) bool        // O(log(n))
+		GetNode(key T) (*Node[T, K], bool) // O(log(n))
+		Get(key T) (K, bool)               // O(log(n))
 
-	InsertOrUpdate(key T, value K)              // O(log(n))
-	InsertMany(entries ...*Entry[T, K])         // O(n * log(k)) k - size
-	InsertOrUpdateMany(entries ...*Entry[T, K]) // O(n * log(k)) k - size
-	RemoveMany(keys ...T)                       // O(n * log(k)) k - size
+		InsertOrUpdate(key T, value K)              // O(log(n))
+		InsertMany(entries ...*Entry[T, K])         // O(n * log(k)) k - size
+		InsertOrUpdateMany(entries ...*Entry[T, K]) // O(n * log(k)) k - size
+		RemoveMany(keys ...T)                       // O(n * log(k)) k - size
 
-	PreOrderFunc(callback func(*Node[T, K]))  // recursive
-	InOrderFunc(callback func(*Node[T, K]))   // recursive
-	PostOrderFunc(callback func(*Node[T, K])) // recursive
+		PreOrderFunc(callback func(*Node[T, K]))  // recursive
+		InOrderFunc(callback func(*Node[T, K]))   // recursive
+		PostOrderFunc(callback func(*Node[T, K])) // recursive
 
-	Iterator() *Iterator[T, K]
+		Iterator() *Iterator[T, K]
 
-	Values() []K                      // recursive
-	PreOrderValues() []K              // recursive
-	PostOrderValues() []K             // recursive
-	Keys() []T                        // recursive
-	PreOrderKeys() []T                // recursive
-	PostOrderKeys() []T               // recursive
-	Entries() []*Entry[T, K]          // recursive
-	PreOrderEntries() []*Entry[T, K]  // recursive
-	PostOrderEntries() []*Entry[T, K] // recursive
+		Values() []K                      // recursive
+		PreOrderValues() []K              // recursive
+		PostOrderValues() []K             // recursive
+		Keys() []T                        // recursive
+		PreOrderKeys() []T                // recursive
+		PostOrderKeys() []T               // recursive
+		Entries() []*Entry[T, K]          // recursive
+		PreOrderEntries() []*Entry[T, K]  // recursive
+		PostOrderEntries() []*Entry[T, K] // recursive
 
-	Right() *Node[T, K]
-	Left() *Node[T, K]
-}
+		Right() *Node[T, K]
+		Left() *Node[T, K]
+	}
+)
 
 var instance interface{} = nil
 
